db82c6a25115fbeda3fa90678d803315136a5694
Oleg Stepura <github@oleg.stepura.com>
2016-12-13 13:22:06 +0200

    Update bash.bashrc.local.sh

--- bash.bashrc.local.sh
+++ bash.bashrc.local.sh
@@ -1,15 +1,5 @@
 # /etc/bash.bashrc
-#
-# https://wiki.archlinux.org/index.php/Color_Bash_Prompt
-#
-# This file is sourced by all *interactive* bash shells on startup,
-# including some apparently interactive shells such as scp and rcp
-# that can't tolerate any output. So make sure this doesn't display
-# anything or bad things will happen !
-
-# Test for an interactive shell. There is no need to set anything
-# past this point for scp and rcp, and it's important to refrain from
-# outputting anything in those cases.
+# source: https://github.com/olegstepura/config-bashrc
 
 # If not running interactively, don't do anything!
 [ -z "$PS1" ] && return
